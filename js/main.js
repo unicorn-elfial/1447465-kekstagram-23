d2d85f88c64ed2940b92e391fd55f06f98e7aafc
Алия <64091297+unicorn-elfial@users.noreply.github.com>
2021-05-29 01:50:15 +0300

    function

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+function maxline (checkedString, maxLength) {
+checkedString <= maxLength;
+return true;
+checkedString > maxLength;
+return false;
+}

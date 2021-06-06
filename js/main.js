c140bdad822fae2ec522109338846d319157e9a9
Алия <64091297+unicorn-elfial@users.noreply.github.com>
2021-06-06 21:29:01 +0300

    детали

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ return false;
 }
 
 
-onst NAMES = [
+const NAMES = [
   'Ира',
   'Вася',
   'Карина',
@@ -25,21 +25,27 @@ let url = 'photos/{{i}}.jpg';
 
  const description = ['милота зашкаливает'];
 const comments = ['Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.']
-const createPhoto = () => {
-  const randomNameIndex = _.random(0, NAMES.length - 1);
+function randomNameIndex (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random(NAMES) * (max - min + 1)) + min; 
+}
 
   function id () {
     Math.floor (math.random () * 25) + 1;
+    return id;
   }
   function likes () {
     Math.floor (math.random () * 200) + 15;
+    return likes;
   }
 
 return {
 id : '',
 avatar : '',
 message : '',
-name: NAMES[randomNameIndex] + ' '
+name: NAMES[randomNameIndex] + ' ',
+likes: ''
 };
 };
 

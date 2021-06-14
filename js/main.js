5b487637ae365a5b1aad3139f807f1b7bce178ba
Алия <64091297+unicorn-elfial@users.noreply.github.com>
2021-06-14 16:23:19 +0300

    больше деталей

--- js/main.js
+++ js/main.js
@@ -1,55 +1,57 @@
+const createPhoto;
 
-function getRandomIntInclusive (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-function maxline (checkedString, maxLength) {
-checkedString <= maxLength;
-return true;
-checkedString > maxLength;
-return false;
+function maxLine(checkedString, maxLength) {
+    if (checkedString <= maxLength) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 const NAMES = [
-  'Ира',
-  'Вася',
-  'Карина',
-  'Петя',
-  'Соня',
-  'Дима',
+    'Ира',
+    'Вася',
+    'Карина',
+    'Петя',
+    'Соня',
+    'Дима',
 ];
-let url = 'photos/{{i}}.jpg';
+const url = 'photos/{{i}}.jpg';
 
- const description = ['милота зашкаливает'];
+const description = ['милота зашкаливает'];
 const comments = ['Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.']
-function randomNameIndex (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random(NAMES) * (max - min + 1)) + min; 
+
+function randomNameIndex(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random(NAMES) * (max - min + 1)) + min;
 }
 
-  function id () {
-    return Math.floor (math.random () * 25) + 1;
-    return id;
-  }
-  function likes () {
-   return Math.floor (math.random () * 200) + 15;
-    return likes;
-  }
+function id() {
+    return Math.floor(math.random() * 25) + 1;
+}
 
-return {
-id : '',
-avatar : '',
-message : '',
-name: NAMES[randomNameIndex] + ' ',
-likes: ''
-};
+function likes() {
+    return Math.floor(math.random() * 200) + 15;
+}
+const createPhoto[
+    id: '',
+    avatar: '',
+    message: '',
+    name: NAMES[randomNameIndex] + ' ',
+    likes: ''
+];
 };
 
 
 console.log(
-  createPhoto()
+    createPhoto
 );
